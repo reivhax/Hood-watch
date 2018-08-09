1e42ee8da6a53d0cc23eaab9ce89ca4be3ee3406
Xavier Kibet <xavierkibet@gmail.com>
2018-08-09 18:18:34 +0300

    Create form for neighbourhood creation

--- hoods/forms.py
+++ hoods/forms.py
@@ -11,3 +11,15 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         exclude = ('user', 'is_admin', 'neighbourhood')
 
+
+class HoodForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Name'].widget = forms.TextInput()
+        self.fields['police'].widget = forms.TextInput()
+        self.fields['health'].widget = forms.TextInput()
+
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
+
