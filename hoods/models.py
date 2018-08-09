bbf86181fc6c22fa9e5ba1edf493d7cd36b3cc9f
Xavier Kibet <xavierkibet@gmail.com>
2018-08-09 18:05:38 +0300

    Use django signals to auto create profile for user

--- hoods/models.py
+++ hoods/models.py
@@ -5,6 +5,17 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
 class Neighbourhood(models.Model):
     Name = models.TextField()
     display = models.ImageField(upload_to='groups/', default='groups/group.png')
@@ -24,3 +35,5 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile {self.user.username}'
+
+
