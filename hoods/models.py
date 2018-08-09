9ba226fdd0f0ec70ed304099d8df5e2b1b3a2417
Xavier Kibet <xavierkibet@gmail.com>
2018-08-09 18:02:09 +0300

    Create neighbourhood model with name and picture.

--- hoods/models.py
+++ hoods/models.py
@@ -5,8 +5,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+class Neighbourhood(models.Model):
+    Name = models.TextField()
+    display = models.ImageField(upload_to='groups/', default='groups/group.png')
 
-# Create your models here.
+    def __str__(self):
+        return self.Name
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     Name = models.TextField(default="Anonymous")
